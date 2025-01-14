9ea128421d5e8cec7322ac70d784f7ba91fb8a15
Ariel Reyes <132673012+ArielReyes04@users.noreply.github.com>
2025-01-14 15:14:24 -0500

    Update exercise.js

--- backend/models/exercise.js
+++ backend/models/exercise.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const exerciseSchema = new mongoose.Schema({
   codewarsId: { type: String, unique: true, required: true },
   difficulty: { type: String, required: true },
-  answer: { type: String, required: true },
+  answer: { type: Map, of: mongoose.Schema.Types.Mixed, required: true },
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
