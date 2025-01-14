f05ea70ab0381d07c3b5838fc2b1193864e34a13
Ariel Reyes <132673012+ArielReyes04@users.noreply.github.com>
2025-01-13 19:34:46 -0500

    Update exerciseRoutes.js

--- backend/routes/exerciseRoutes.js
+++ backend/routes/exerciseRoutes.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { createExercise } = require('../controllers/exerciseController');
+const { createExercise, getExercises, getExerciseById, deleteExercise } = require('../controllers/exerciseController');
 
-router.post('/exercise', createExercise);
+// Crear ejercicio
+router.post('/exercises', createExercise);
+
+// Obtener todos los ejercicios
+router.get('/exercises', getExercises);
+
+// Obtener un ejercicio por su ID
+router.get('/exercises/:id', getExerciseById);
+
+// Eliminar un ejercicio por su ID
+router.delete('/exercises/:id', deleteExercise);
 
 module.exports = router;
