85c85988add4ba510e6e86e192a2ae1cb7ec86ee
AvilesDanie <daaviles3@espe.edu.ec>
2025-01-18 17:30:50 -0500

    adaptaacion

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { createUser, getUsers, getUserById, deleteUser, updateUser, updateUserProgress } = require('../controllers/userController');
+const { createUser, getUsers, getUserById, deleteUser, updateUser, updateUserProgressUnified } = require('../controllers/userController');
+
+// Actualizar progreso de un usuario
+router.put('/users/progress-unified', updateUserProgressUnified);
 
 // Crear usuario
 router.post('/users', createUser);
@@ -17,7 +20,6 @@ router.delete('/users/:id', deleteUser);
 // Actualizar un usuario
 router.put('/users/:id', updateUser);
 
-// Actualizar progreso de un usuario
-router.put('/users/progress/:id', updateUserProgress);
+
 
 module.exports = router;
--- frontend/src/components/Ejercicio.js
+++ frontend/src/components/Ejercicio.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Ejercicio = ({ ejercicio, onSeleccionar }) => {
   return (
-    <div onClick={onSeleccionar} style={{ cursor: "pointer", marginBottom: "20px" }}>
-      <h3>{ejercicio.titulo}</h3>
-      <p>{ejercicio.descripcion.substring(0, 100)}...</p>
+    <div
+      onClick={onSeleccionar}
+      style={{ cursor: "pointer", marginBottom: "20px" }}
+    >
+      <h3>{ejercicio.name}</h3>
+      <p>{ejercicio.description.substring(0, 100)}...</p>
     </div>
   );
 };
